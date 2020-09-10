cdaee0ff85f886b721bb7b520b1f1882cd78d3b1
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-10 02:32:30 -0700

    Add config update to script

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-# Step 1:
-# This is your Docker ID/path
-dockerpath=$(<./variables/path.txt)
-dockerimage=$(<./variables/name.txt)
+## Step 1:
+## This is your Docker ID/path
+#dockerpath=$(<./variables/path.txt)
+#dockerimage=$(<./variables/name.txt)
 
-# Step 2
-# Run the Docker Hub container with kubernetes
-kubectl run $dockerimage\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=$dockerimage
-
+## Step 2
+## Run the Docker Hub container with kubernetes
+#kubectl run $dockerimage\
+#    --generator=run-pod/v1\
+#    --image=$dockerpath\
+#    --port=80 --labels app=$dockerimage
+########################################
+aws eks --region $(<./variables/region.txt) update-kubeconfig --name $(<./variables/name.txt)
+kubectl config use-context $(</tmp/cluster_ARN.txt)
+kubectl apply -f ./deployment.yml
+########################################
 # Wait for pod creation to complete
 sleep 20s
 
