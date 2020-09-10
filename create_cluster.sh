d4443651a5034175b6ae53fb9d38c055baf98ce8
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-10 00:43:44 -0700

    Correct indentation at lines 13,14 and 15

--- create_cluster.sh
+++ create_cluster.sh
@@ -10,9 +10,9 @@
 ## mkdir -p /tmp/jenkins_build
  # Write cluster ARN to file
  aws cloudformation describe-stacks \
- --region $(<./variables/region.txt) --stack-name eksctl-$(<./variables/name.txt)-cluster \ 
- --query "Stacks[0].Outputs[?OutputKey=='ARN'].OutputValue" \
- --output text>/tmp/cluster_ARN.txt
+  --region $(<./variables/region.txt) --stack-name eksctl-$(<./variables/name.txt)-cluster \ 
+  --query "Stacks[0].Outputs[?OutputKey=='ARN'].OutputValue" \
+  --output text>/tmp/cluster_ARN.txt
  
   # prefixed "eksctl" and appended "cluster" to match cloudformation describe-stack StackName naming pattern \
   
