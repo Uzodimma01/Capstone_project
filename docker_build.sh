a6e70bbe1d0a15674126366383d57727e4b37af2
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-09 23:43:11 -0700

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -10,4 +10,4 @@ docker image ls
 
 # Step 3: 
 # Run app
-docker run -it --rm -d -p 80:80 $(<./variables/name.txt)>$(./variables/output_dir.txt)/docker_container_Id.txt
+docker run -it --rm -d -p 80:80 $(<./variables/name.txt)>/tmp/docker_container_Id.txt
