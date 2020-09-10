e38c46a3777612a4107cce5b06b0fc06e9afa6ab
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-10 04:31:11 -0700

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,13 +1,13 @@
-##eksctl create cluster \
-## --name $(<./variables/name.txt) \
-## --version 1.17 \
-## --without-nodegroup
+eksctl create cluster \
+ --name $(<./variables/name.txt) \
+ --version 1.17 \
+ --without-nodegroup
  
  # This is not part of a standard 
  # create cluster syntax
  
  # create a dir to write files to
-## mkdir -p /tmp/jenkins_build
+ mkdir -p /tmp/jenkins_build
  # Write cluster ARN to file
  aws cloudformation describe-stacks \
   --region $(<./variables/region.txt) --stack-name eksctl-$(<./variables/name.txt)-cluster --query "Stacks[0].Outputs[?OutputKey=='ARN'].OutputValue"  --output text>/tmp/cluster_ARN.txt
