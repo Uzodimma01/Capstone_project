736372093517da5262c4f5183bca8160d330fc42
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-10 16:00:14 -0700

    Update create_nodes.sh

--- create_nodes.sh
+++ create_nodes.sh
@@ -10,3 +10,16 @@ eksctl create nodegroup \
 --nodes-max $(<./variables/max_node.txt) \
 #--ssh-access \
 #--ssh-public-key $(<./variables/access_key.txt)
+
+ # This is not part of a standard 
+ # create cluster syntax
+ 
+ # create a dir to write files to
+ mkdir -p /tmp/jenkins_build
+ # Write cluster ARN to file
+ aws cloudformation describe-stacks \
+  --region $(<./variables/region.txt) --stack-name eksctl-$(<./variables/name.txt)-nodegroup-$(<./variables/name.txt) --query "Stacks[0].Outputs[?OutputKey=='ARN'].OutputValue"  --output text>/tmp/cluster_ARN.txt
+ # prefixed "eksctl" and appended "cluster" to match cloudformation describe-stack StackName naming pattern 
+  
+ cat /tmp/cluster_ARN.txt
+
