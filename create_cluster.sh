30889389bb1c1ba6c89f6c2fdf682357f18fdd5c
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-10 02:54:14 -0700

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -10,8 +10,7 @@
 ## mkdir -p /tmp/jenkins_build
  # Write cluster ARN to file
  aws cloudformation describe-stacks \
-  --region $(<./variables/region.txt) --stack-name eksctl-$(<./variables/name.txt)-cluster --query "Stacks[0].Outputs[?OutputKey=='ARN'].OutputValue" \
-   # prefixed "eksctl" and appended "cluster" to match cloudformation describe-stack StackName naming pattern 
-  --output text>/tmp/cluster_ARN.txt
+  --region $(<./variables/region.txt) --stack-name eksctl-$(<./variables/name.txt)-cluster --query "Stacks[0].Outputs[?OutputKey=='ARN'].OutputValue"  --output text>/tmp/cluster_ARN.txt
+ # prefixed "eksctl" and appended "cluster" to match cloudformation describe-stack StackName naming pattern 
   
  cat /tmp/cluster_ARN.txt
